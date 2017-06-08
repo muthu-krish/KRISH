c7753d302cb7df25835f4be981be470e159e3118
muthu-krish <muthukrishnan888it@gmail.com>
2017-06-08 12:23:57 +0530

    Create rev.c

---
+++ rev.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+
+void main()
+{
+   int i, j, k;
+   char str[100];
+   char rev[100];
+   
+   printf("Enter a string\t");
+   scanf("%s", str);
+   printf("The original string is %s", str);
+   for(i = 0; str[i] != '\0'; i++);
+   {
+      k = i-1;
+   }
+   for(j = 0; j <= i-1; j++)
+   {
+      rev[j] = str[k];
+      k--;
+   }
+   printf("The reverse string is %s", rev);
+
+
+}
+
