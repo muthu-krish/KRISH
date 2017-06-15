809ff919f71a7669bdaee4e4c02e38cb85c2a7b2
muthu-krish <muthukrishnan888it@gmail.com>
2017-06-15 14:32:27 +0530

    Create Str.java

---
+++ Str.java
@@ -0,0 +1,8 @@
+ import java.io.*;
+    public class Str{  
+    public static void main(String args[]){  
+    String s="200";  
+    int i=Integer.parseInt(s);  
+    System.out.println(s+100); 
+    System.out.println(i+100);  
+    }}  
